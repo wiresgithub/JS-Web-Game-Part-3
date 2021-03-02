8d23910317df9767551a8f31a66c850b3118d0a5
Joshua Miles <joshua.m@hackerusa.com>
2021-03-02 14:29:13 -0600

    -

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
-function newImage(url, left, bottom){
+function newImage(url){
     let image = document.createElement('img')
     image.src = url
-    image.style.position = 'fixed'
-    image.style.left = left + 'px'
-    image.style.bottom = bottom + 'px'
     document.body.append(image)
     return image
 }
 
-function newItem(url, left, bottom){
-    let item = newImage(url, left, bottom)
+function newItem(url){
+    let item = newImage(url)
     item.addEventListener('click', () => {
         item.remove()
         let inventoryItem = document.createElement('img')
         inventoryItem.src = url;
         inventory.append(inventoryItem)
     })
+    return item
 }
 
 function makeInventory(){
@@ -35,15 +33,24 @@ function makeInventory(){
     return inventory
 }
 
+function move(image){
+    image.style.position = 'fixed'
+    return {
+        to: function(left, bottom){
+            image.style.left = left + 'px'
+            image.style.bottom = bottom + 'px'
+        }
+    }
+}
 
 const inventory = makeInventory()
-newImage('assets/green-character.gif', 100, 250)
-newImage('assets/tree.png', 200, 450)
-newImage('assets/pillar.png', 350, 250)
-newImage('assets/pine-tree.png', 450, 350)
-newImage('assets/crate.png', 150, 350)
-newImage('assets/well.png', 500, 575)
+move(newImage('assets/green-character.gif')).to(100, 250)
+move(newImage('assets/tree.png')).to(200, 450)
+move(newImage('assets/pillar.png')).to(350, 250)
+move(newImage('assets/pine-tree.png')).to(450, 350)
+move(newImage('assets/crate.png')).to(150, 350)
+move(newImage('assets/well.png')).to(500, 575)
 
-newItem('assets/sword.png', 500, 555)
-newItem('assets/shield.png', 165, 335)
-newItem('assets/staff.png', 600, 250)
+move(newItem('assets/sword.png')).to(500, 555)
+move(newItem('assets/shield.png')).to(165, 335)
+move(newItem('assets/staff.png')).to(600, 250)
